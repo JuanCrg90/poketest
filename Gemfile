fcd44057f7bbcd81eaf1bf30380247731a0061a2
Juan C. Ruiz <JuanCrg90@gmail.com>
2022-05-26 21:49:03 -0500

    Configure VCR gem
    we want to use VCR gem to record requests, this adds the basic
    configuration
    
    https://github.com/vcr/vcr

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ group :test do
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
   gem 'mocha'
+  gem 'webmock'
+  gem 'vcr'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 require 'mocha/minitest'
+require 'vcr'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -11,4 +12,8 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  VCR.configure do |config|
+    config.cassette_library_dir = "fixtures/vcr_cassettes"
+    config.hook_into :webmock
+  end
 end
